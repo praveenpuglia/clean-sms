4ada772871215add4df79cc7ee304284aa70cd11
Praveen Puglia <praveen.puglia@smallcase.com>
2025-10-24 22:42:09 +0530

    feat: conversion to material 3 dynamic colors

--- app/src/main/java/com/praveenpuglia/cleansms/MyApplication.kt
+++ app/src/main/java/com/praveenpuglia/cleansms/MyApplication.kt
@@ -7,6 +7,8 @@ class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         // Enable Material You dynamic colors based on system wallpaper
+        // This will apply dynamic colors on Android 12+ (API 31+)
+        // and fall back to the theme colors on older versions
         DynamicColors.applyToActivitiesIfAvailable(this)
     }
 }
