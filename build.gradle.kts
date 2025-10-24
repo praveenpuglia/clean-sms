befaf70cbac95cc7a629bfdc2a6ec7b9709e1a3a
Praveen Puglia <praveen.puglia@smallcase.com>
2025-10-24 14:32:01 +0530

    chore: move to java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Java version is set in app/build.gradle.kts (Java 21)
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
